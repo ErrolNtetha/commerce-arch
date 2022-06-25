d1bb34b3e721c74d6d587e41f45049a41d019775
elregalo <giftmoobi@gmail.com>
2022-06-25 11:10:05 +0200

    fixed logs dir location

--- util/logger.js
+++ util/logger.js
@@ -3,19 +3,20 @@
  * Date: 2022-06-22
  * Project: commerce-arch/util/logger.js
  * Description: Lib for logger. This lib is used to log messages
- * Function: errLog (message) - Logs a success message
- * Function: successLog (message) - Logs an error message
+ * Function: errorLog (message) - Logs a error message
+ * Function: successLog (message) - Logs an success message
  */
-import winston from "winston";
 import "winston-daily-rotate-file";
+import winston from "winston";
 
-export const errLog = winston.createLogger({
+
+export const errorLog = winston.createLogger({
   name: "error",
   level: "error",
   json: true,
   transports: [
     new winston.transports.DailyRotateFile({
-      filename: "../logs/error-%DATE%.log",
+      filename: "./logs/error-%DATE%.log",
       datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
       maxSize: "20m",
@@ -30,7 +31,7 @@ export const successLog = winston.createLogger({
   json: true,
   transports: [
     new winston.transports.DailyRotateFile({
-      filename: "../logs/success-%DATE%.log",
+      filename: "./logs/success-%DATE%.log",
       datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
       maxSize: "20m",
