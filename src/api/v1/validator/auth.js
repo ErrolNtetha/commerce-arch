d86b0a84a5c3536e5e3f5264f340dd254b63e628
elregalo <giftmoobi@gmail.com>
2022-06-26 01:16:42 +0200

    added auth validator

---
+++ src/api/v1/validator/auth.js
@@ -0,0 +1,51 @@
+/*
+ * Author: Moobi Kabelo
+ * Date: 2022-06-22
+ * Project: commerce-arch/helpers/auth.js
+ * Description: Helper for authentication. This helper is used to validate the user's credentials (email and password)
+ * Function: comparePassword (password, hashedPassword) <Promise> - Decrypt password in database and compare it with password provided by password
+ * Function: validateEmail (email)  <Promise> - Validates the email address provided by the user and returns error if not valid
+ * Function validateMobile (mobile) <Promise> - Adds country code to mobile
+ * Function: validatePassword (password, confirmPassword) <Promise> - Validates the passwords provided by the user and returns the
+ *                                                          confirmPassword if is valid and returns a hashed password
+ */
+import validator from "validator";
+
+export const validateEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    if (validator.isEmail(email)) {
+      resolve();
+    } else {
+      reject(new Error("Sorry, invalid email."));
+    }
+  });
+};
+
+export const validateMobile = (mobile) => {
+  if (mobile[0] === "0") {
+    return mobile.replace(mobile.indexOf("0"), "+27").replace(/\s/g, "");
+  }
+  return mobile;
+};
+
+export const validatePassword = (password, confirmPassword) => {
+  return new Promise((resolve, reject) => {
+    if (password !== confirmPassword) {
+      reject(new Error("Sorry, passwords does not match."));
+    } else if (password.length < 8) {
+      reject(new Error("Sorry, password must be at least 8 characters long."));
+    } else if (
+      !password.match(/[a-z]/g) ||
+      !password.match(/[A-Z]/g) ||
+      !password.match(/[0-9]/g)
+    ) {
+      reject(
+        new Error(
+          "Password must contain at least one lowercase letter, one uppercase letter, and one number."
+        )
+      );
+    } else {
+      resolve();
+    }
+  });
+};
