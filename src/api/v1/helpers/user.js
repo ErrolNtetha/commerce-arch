ff34486e8e7ae3d198fd2cb1bdb5501e74dea383
elregalo <giftmoobi@gmail.com>
2022-06-27 01:01:25 +0200

    added find user by id

--- src/api/v1/helpers/user.js
+++ src/api/v1/helpers/user.js
@@ -1,16 +1,14 @@
-/*
- * Author: Moobi Kabelo
- * Date: 2022-26-24
- * Project commerce-arch/services/auth.services.js
- * Description: Services for authentication. This service is used to do CRUD operations on User model
- * Function: findUser (email) <Promise> - Checks if user already exists in database
- * Function: deleteUser (userId) <Promise> - Deletes user from database
- * Function: updateUser <Promise> -  Updated users details in database
- * Function: saveUser <Promise> - Adds new user to database
- */
 import { errorLog } from "../../../util/logger.js";
 import User from "../models/user.js";
 
+/**
+ * It creates a new user and saves it to the database
+ * @param fullNames - The full names of the user.
+ * @param email - The email address of the user.
+ * @param mobile - The user's mobile number.
+ * @param password - The password that the user entered.
+ * @returns A promise
+ */
 export const saveUser = (fullNames, email, mobile, password) => {
   return new Promise((resolve, reject) => {
     const newUser = new User({
@@ -32,6 +30,14 @@ export const saveUser = (fullNames, email, mobile, password) => {
   });
 };
 
+/**
+ * "Find a user by email or mobile number."
+ *
+ * The function takes two parameters: email and mobile
+ * @param email - The email address of the user.
+ * @param mobile - {
+ * @returns A promise
+ */
 export const findUser = (email, mobile) => {
   return User.findOne({
     $or: [
@@ -49,6 +55,15 @@ export const findUser = (email, mobile) => {
   });
 };
 
+export const findUserById = (id) => {
+  return User.findById(id);
+};
+
+/**
+ * It finds a user by their id and deletes them
+ * @param userId - The id of the user to delete.
+ * @returns The user object that was deleted.
+ */
 export const deleteUser = (userId) => {
   return User.findByIdAndDelete(userId);
 };
