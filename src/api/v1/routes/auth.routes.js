e1c05ccd766bb67a8b5353ea171a0bf95b394d5b
elregalo <giftmoobi@gmail.com>
2022-06-27 01:04:51 +0200

    added route

--- src/api/v1/routes/auth.routes.js
+++ src/api/v1/routes/auth.routes.js
@@ -4,11 +4,18 @@ import {
   createAccountLimiter,
   passwordResetLimiter,
 } from "../middleware/limiter.js";
-import { signIn, signUp, resetPassword } from "../controllers/auth.js";
+import {
+  signIn,
+  signUp,
+  resetPassword,
+  verifyOtp,
+} from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/register", createAccountLimiter, signUp);
 router.get("/login", authLimiter, signIn);
 router.post("/reset-password", passwordResetLimiter, resetPassword);
+router.post("/verify-otp/:id", verifyOtp);
+
 export { router as authRoutes };
