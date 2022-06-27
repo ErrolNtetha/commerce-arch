20c937f3d91612710a8c140b0f8cbc4796255253
elregalo <giftmoobi@gmail.com>
2022-06-27 14:32:00 +0200

    added validator auth tests

--- test/validator/auth.test.js
+++ test/validator/auth.test.js
@@ -1,5 +1,6 @@
 import { validator } from "../../src/api/v1/validator/index.js";
 
+/* Testing the email validation function. */
 test("check if email is valid", () => {
   return expect(validator.validateEmail("giftmoobi@gmail.com")).resolves.toBe(
     "giftmoobi@gmail.com"
@@ -7,7 +8,15 @@ test("check if email is valid", () => {
 });
 
 test("reject if email is invalid", () => {
-  return expect(validator.validateEmail("giftmoobi@@gmail.com")).rejects.toMatch(
-    "Sorry, invalid email."
-  );
+  return expect(
+    validator.validateEmail("giftmoobi@@gmail.com")
+  ).rejects.toMatch("Sorry, invalid email.");
+});
+
+test("add country code to mobile if not exist", () => {
+  expect(validator.validateMobile("0771234567")).toBe("+27771234567");
+});
+
+test("return mobile if country code exist", () => {
+  expect(validator.validateMobile("+27771234567")).toBe("+27771234567");
 });
